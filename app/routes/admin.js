fbcd305cde6abbc9378f5a9d7c7592409a61d330
Daniel <daniel.xds@hotmail.com>
2022-04-14 20:11:19 -0300

    inserindo dados no banco de dados usando o método post

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,27 @@
-module.exports = function(app){
-    app.get('/formulario_inclusao_noticia', function(req, res){
+module.exports = function(application){
+    application.get('/formulario_inclusao_noticia', function(req, res){
         res.render('admin/form_add_noticia');
     });
 
     // a função abaixo é a rota para salvar os dados no bando 
-    app.post('/noticias/salvar', function(req, res){
-        var noticias = req.body;
-        res.send(noticias);
-    })
+    application.post('/noticias/salvar', function(req, res){
+        var noticia = req.body;
+        // res.send(noticias);
+
+        // ETAPAS A SEREM FEITAS PARA INSERIR DADOS NO BANCO DE DADOS
+
+        // 1° recuperar a conexão
+        var connection = application.config.dbConnection();
+        var noticiasModel = application.app.models.noticiasModel;
+
+        // 2º recuperar a model
+        noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+            // a linha abaixo retorna para a rota que exibe as noticias
+            //res.render("noticias/noticias", {noticia : result });
+            // a linha abaixo permite redirecionar a rota
+            res.redirect('/noticias');
+        });
+
+        // enviar os dados para a função que irá salvar as informações 
+    });
 }
