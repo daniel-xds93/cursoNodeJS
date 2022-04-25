16b92cb376df5365984e23f0b119f1b20b158485
Daniel <daniel.xds@hotmail.com>
2022-04-25 16:12:38 -0300

    implementando controllers no projeto e trabalhando com rotas, passando apenas os parametros

--- prj06_implementando-controllers/app/routes/home.js
+++ prj06_implementando-controllers/app/routes/home.js
@@ -1,6 +1,5 @@
 module.exports = function(app){
     app.get('/', function(req, res){
-        // o método render só aparece por conta da instalação do pacote ejs
-        res.render('home/index');
+        app.app.controllers.home.index(app, req, res);
     });
 };
