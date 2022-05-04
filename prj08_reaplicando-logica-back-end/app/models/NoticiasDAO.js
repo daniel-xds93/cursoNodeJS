1a49bd8b2335e2c661959e0592e3095c5392d88c
Daniel <daniel.xds@hotmail.com>
2022-05-04 15:31:59 -0300

    listando as noticias da mais atual para mais antica

--- prj08_reaplicando-logica-back-end/app/models/NoticiasDAO.js
+++ prj08_reaplicando-logica-back-end/app/models/NoticiasDAO.js
@@ -6,7 +6,7 @@ function NoticiasDAO(connection){
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback){
-    this._connection.query('select * from noticias', callback);
+    this._connection.query('select * from noticias order by data_criacao desc', callback);
 }
 
 NoticiasDAO.prototype.getNoticia = function(callback){
