d3968c0372ed8a0a9794584b6542816818edca2f
Daniel <daniel.xds@hotmail.com>
2022-05-05 16:11:38 -0300

    reformulando as rotas aplicando controllers

--- mutiroom_chat/prj04_ajustando_routes_e_controllers/app/routes/index.js
+++ mutiroom_chat/prj04_ajustando_routes_e_controllers/app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(application){
     application.get('/', function(req, res){
-        res.render('index');
+        // a linha abaixo é o caminho ate chegar na função q esta na index dentro da pasta controllers
+        application.app.controllers.index.home(application, req, res);
     });
 }
